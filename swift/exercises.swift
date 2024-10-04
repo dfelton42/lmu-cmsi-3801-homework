fd8c60931764f6f56dfd77815147a689d3ba036c
Stan319 <89944605+Stan319@users.noreply.github.com>
2024-10-03 20:48:19 -0700

    Update exercises.swift
    added meaningful line count and binary search tree

--- swift/exercises.swift
+++ swift/exercises.swift
@@ -46,7 +46,87 @@ func say(_ word: String = "") -> Say {
     return Say(word)
 }
 // Write your meaningfulLineCount function here
-
+func meaningfulLineCount(_ filename: String) async -> Result<Int, Error> {
+    do {
+        guard let fileHandle = FileHandle(forReadingAtPath: filename) else {
+            return .failure(NoSuchFileError())
+        }
+        defer { fileHandle.closeFile() }
+        
+        var count = 0
+        for try await line in fileHandle.bytes.lines {
+            if !line.isEmpty {
+                count += 1
+            }
+        }
+        return .success(count)
+    } catch {
+        return .failure(error)
+    }
+}
 // Write your Quaternion struct here
 
 // Write your Binary Search Tree enum here
+enum BinarySearchTree: CustomStringConvertible, Equatable {
+    case empty
+    indirect case node(BinarySearchTree, String, BinarySearchTree)
+    
+    var size: Int {
+        switch self {
+        case .empty:
+            return 0
+        case let .node(left, _, right):
+            return 1 + left.size + right.size
+        }
+    }
+    
+    func contains(_ value: String) -> Bool {
+        switch self {
+        case .empty:
+            return false
+        case let .node(left, element, right):
+            if value == element {
+                return true
+            } else if value < element {
+                return left.contains(value)
+            } else {
+                return right.contains(value)
+            }
+        }
+    }
+    
+    func insert(_ value: String) -> BinarySearchTree {
+        switch self {
+        case .empty:
+            return .node(.empty, value, .empty)
+        case let .node(left, v, right):
+            if value < v {
+                return .node(left.insert(value), v, right)
+            } else if value > v {
+                return .node(left, v, right.insert(value))
+            } else {
+                return self
+            }
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .empty:
+            return "()"
+        case let .node(left, value, right):
+            return "(\(left)\(value)\(right))"
+        }
+    }
+    
+    static func == (lhs: BinarySearchTree, rhs: BinarySearchTree) -> Bool {
+        switch (lhs, rhs) {
+        case (.empty, .empty):
+            return true
+        case let (.node(left1, value1, right1), .node(left2, value2, right2)):
+            return value1 == value2 && left1 == left2 && right1 == right2
+        default:
+            return false
+        }
+    }
+}
